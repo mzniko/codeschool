5c27f2ebf348ca38d2b1ef9d075d532da7b9ab0e
Marko Nikolovski <mz.nikolovski@gmail.com>
2014-02-09 23:36:59 -0800

    classy scaffolding for analyze.rb

--- code_challenge_02/txt2csv/lib/analyze.rb
+++ code_challenge_02/txt2csv/lib/analyze.rb
@@ -1,1 +1,36 @@
-# put your solution to code challenge 02 here
+class Analyze
+  def initialize
+    @param = ARGV.shift
+  end
+
+  def read_arg
+    case @param
+    when '-p'
+      prefix_regex
+    when '-s'
+      suffix_regex
+    else
+     abort("usage: " + __FILE__ + " [-p | -s] < [filename]")
+    end
+  end
+
+  def prefix_regex
+    puts 'selected prefix_regex'
+    #not implemented
+  end
+
+  def suffix_regex
+    puts 'selected suffix_regex'
+    #not implemented
+  end
+
+  def read_lines
+    ARGF.each_line do |line|
+      puts line
+    end
+  end
+end
+
+run = Analyze.new
+run.read_arg
+#run.read_lines
