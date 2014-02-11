9382ddf849ef1797bf3d65f82bbd479b16984472
Marko Nikolovski <mz.nikolovski@gmail.com>
2014-02-10 19:38:59 -0800

    finished analyze class

--- code_challenge_02/txt2csv/lib/analyze.rb
+++ code_challenge_02/txt2csv/lib/analyze.rb
@@ -1,36 +1,38 @@
+require 'pp'
+
 class Analyze
+  attr_reader :regex_hash
+
   def initialize
     @param = ARGV.shift
+    @text = STDIN
+    @regex_hash = Hash.new(0)
   end
 
-  def read_arg
+  def analyze
     case @param
     when '-p'
-      prefix_regex
+      @text.each_line { |line| prefix_regex(line) }
     when '-s'
-      suffix_regex
+      @text.each_line { |line| suffix_regex(line) }
     else
      abort("usage: " + __FILE__ + " [-p | -s] < [filename]")
     end
   end
 
-  def prefix_regex
-    puts 'selected prefix_regex'
-    #not implemented
+  def prefix_regex(line)
+    prefix = /^\w+/.match(line).to_s
+    @regex_hash[:"#{prefix}"] += 1
   end
 
-  def suffix_regex
-    puts 'selected suffix_regex'
-    #not implemented
-  end
-
-  def read_lines
-    ARGF.each_line do |line|
-      puts line
-    end
+  def suffix_regex(line)
+    suffix_array = line.scan(/(?!x)([a-z]+\.*)/i)
+    suffix_array.flatten!
+    suffix = suffix_array.last
+    @regex_hash[:"#{suffix}"] += 1
   end
 end
 
 run = Analyze.new
-run.read_arg
-#run.read_lines
+run.analyze
+pp run.regex_hash
